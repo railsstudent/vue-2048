536f149c6517b5d26e0cf7c790f79c3409bdf9e2
Connie Leung <cleung334@gmail.com>
2018-09-10 01:56:37 +0800

    Update unit tests and add e2e test
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,14 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
+describe("2048 game", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    cy.contains("p.title", "Vue 2048 Game");
+    cy.contains("p.footer", "Made by Connie @ 2018");
+    cy.contains("button.btn", "Start Game");
+    const scores = cy.get("div.stats");
+    cy.contains(scores[0], "Num Moves: 0");
+    cy.contains(scores[1], "Current highest tile: -1");
+    cy.contains(scores[2], "Score: 0");
   });
 });
